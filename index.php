753a4a1ae8e27f3bdcddd24493047f371c4a3b0a
Magic-Mayo <mike.mayoii@gmail.com>
2020-09-28 11:31:17 -0700

    add sql link and start functions

--- index.php
+++ index.php
@@ -7,19 +7,28 @@
 </head>
 <body>
     <?php
+        // connection to mysql db
+        $sql_link = mysqli_connect("hostname", "username", "password", "database");
 
         // function displayComments($toSearch) for displaying comments with param for what to search for in string
+        function displayComments($toSearch){
             // get data from table with param in string
             // use data to display all comments that have the string to search
+            // return 
             // run function that updates date by sending array of order numbers
+        }
 
         // function updateShipDate($orders) to update shipdate_expected column
+        function updateShipDate($orders){
             // loop param and check:
                 // shipdate_expected column and update only if it is set to (0000-00-00 00:00:00), continue loop if already set
                 // check comment for expected shipdate. if exists, set update query to new date
+        }
 
 
         // Comments about candy
+        displayComments('candy');
+
         // Comments about call me / don't call me
         // Comments about who referred me
         // Comments about signature requirements upon delivery
