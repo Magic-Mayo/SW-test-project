b53e7745cbab2f72e5a843a17b7deb536a44d1d1
Magic-Mayo <mike.mayoii@gmail.com>
2020-09-28 11:22:20 -0700

    updated with comments on initial steps of how to start project

--- index.php
+++ index.php
@@ -6,6 +6,24 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+
+        // function displayComments($toSearch) for displaying comments with param for what to search for in string
+            // get data from table with param in string
+            // use data to display all comments that have the string to search
+            // run function that updates date by sending array of order numbers
+
+        // function updateShipDate($orders) to update shipdate_expected column
+            // loop param and check:
+                // shipdate_expected column and update only if it is set to (0000-00-00 00:00:00), continue loop if already set
+                // check comment for expected shipdate. if exists, set update query to new date
+
+
+        // Comments about candy
+        // Comments about call me / don't call me
+        // Comments about who referred me
+        // Comments about signature requirements upon delivery
+        // Miscellaneous comments (everything else)
+    ?>
 </body>
 </html>
